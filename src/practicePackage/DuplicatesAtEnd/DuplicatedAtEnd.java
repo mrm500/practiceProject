92b1405edf4f55b88dc8d067d0b9ca0c63c0c911
mrm <>
2018-07-04 13:45:53 +0530

    Added solution for DuplicatedAtEnd - puts all the duplicates in a List in the end

--- src/practicePackage/DuplicatesAtEnd/DuplicatedAtEnd.java
+++ src/practicePackage/DuplicatesAtEnd/DuplicatedAtEnd.java
@@ -11,20 +11,36 @@ public class DuplicatedAtEnd {
         list.add("alfa");
         list.add("delta");
         list.add("charlie");
+        list.add("echo");
         list.add("alfa");
+        list.add("foxtrot");
         list.add("alfa");
         list.add("charlie");
         list.add("bravo");
         list.add("echo");
-
         List<String> result = sort(list);
+        for (String s : result)
+            System.out.println(s);
     }
 
     private static <T extends Comparable<T>> List<T> sort(List<T> list) {
         list.sort((T a, T b) -> a.compareTo(b));
-        for (T t : list)
-            System.out.println(t);
-        //Pair<Integer, List<T>> pairs = new Pair<>();
+        int indexOfLastUnique = 0;
+        boolean flag = false;
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).compareTo(list.get(i + 1)) < 0) {
+                if (indexOfLastUnique > 0 && list.get(indexOfLastUnique - 1).compareTo(list.get(i + 1)) >= 0){
+                    indexOfLastUnique--;
+                    list.add(i+1, list.get(indexOfLastUnique));
+                    list.remove(indexOfLastUnique);
+                }
+                else {
+                    list.add(indexOfLastUnique, list.get(i + 1));
+                    list.remove(i + 2);
+                    indexOfLastUnique++;
+                }
+            }
+        }
         return  list;
     }
 }
